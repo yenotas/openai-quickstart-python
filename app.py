24f9ae930af4cdb6eb305fbabc9552271feb92f0
yenotas <yenotas@gmail.com>
2023-01-16 06:07:04 +0100

    +img

--- app.py
+++ app.py
@@ -7,31 +7,46 @@ from flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-python = {"top_p": 0.9, "frequency_penalty":0.5, "presence_penalty":0.5, "temperature":0.7, "max_tokens":3000}
-java = {"top_p": 1.0, "frequency_penalty":0.2, "presence_penalty":0.2, "temperature":0.7, "max_tokens":3000}
-text = {"top_p": 1.0, "frequency_penalty":0.0, "presence_penalty":0.0, "temperature":0.5, "max_tokens":2000}
-js = {"top_p": 1.0, "frequency_penalty":0.0, "presence_penalty":0.0, "temperature":0.7, "max_tokens":3000}
-img = {"top_p": 1.0, "frequency_penalty":0.0, "presence_penalty":0.0, "temperature":0.7, "max_tokens":3000, "model":"image-davinci-003"}
-model = {"python":python, "java":java, "text":text, "js":js}
+python = {"top_p": 0.9, "frequency_penalty":0.5, "presence_penalty":0.5, "temperature":0.7, "max_tokens":3000, "model":"text-davinci-003"}
+java = {"top_p": 1.0, "frequency_penalty":0.2, "presence_penalty":0.2, "temperature":0.7, "max_tokens":3000, "model":"text-davinci-003"}
+text = {"top_p": 1.0, "frequency_penalty":0.0, "presence_penalty":0.0, "temperature":0.5, "max_tokens":2000, "model":"text-davinci-003"}
+js = {"top_p": 1.0, "frequency_penalty":0.0, "presence_penalty":0.0, "temperature":0.7, "max_tokens":3000, "model":"text-davinci-003"}
+img = {"top_p": 1.0, "frequency_penalty":0.0, "presence_penalty":0.0, "temperature":0.7, "max_tokens":50, "model":"openai-gpt"}
+model = {"python":python, "java":java, "text":text, "js":js, "img":img}
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
 
-        q = request.form["query"]
-        m = request.form["model"]
+        q = str(request.form["query"])
+        m = str(request.form["model"])
+        link = "link"
+        result = "img"
+        
+        if m == "img":
+            response = openai.Image.create(
+            prompt=q,
+            n=1,
+            size="512x512"
+            )
+            link = str(response['data'][0]['url'])       
+            print("URL =======\n"+link)
+            return render_template("index.html", query=q, link=link)
 
-        response = openai.Completion.create(
-            model="text-davinci-003",
+        else:
+            print(m + " =======")
+            response = openai.Completion.create(
+            model=model[m]["model"],
             prompt=q,
             temperature=model[m]["temperature"],
             max_tokens=model[m]["max_tokens"],
             top_p=model[m]["top_p"],
             frequency_penalty=model[m]["frequency_penalty"],
             presence_penalty=model[m]["presence_penalty"]
-        )
-        return redirect(url_for("index", query=q, result=str(response.choices[0].text)))
+            )
+            result = str(response.choices[0].text)
+            return redirect(url_for("index", query=q, result=result))
 
     result = request.args.get("result")
-    query = request.args.get("query")
-    return render_template("index.html", query=query, result=result)
+    q = request.args.get("query")
+    return render_template("index.html", query=q, result=result)
